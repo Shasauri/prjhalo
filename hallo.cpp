ca050c17ef84fb48da341b7cca898dd577836323
Satya Permana Alan <spermana.alan@gmail.com>
2025-05-23 05:57:41 +0800

    Memasukkan data ke array string

--- hallo.cpp
+++ hallo.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main(){
     char nama[5][20];
+    int i;
+
+    for(i=0;i<4;i++){
+        cout<<"Masukkan data "<<i+1 <<"=";
+        cin>>nama[i];
+    }
     return 0;
 }
