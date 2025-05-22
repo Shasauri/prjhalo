7fd43ca562d59e197d6dc6caecaa798c9846fea9
Satya Permana Alan <spermana.alan@gmail.com>
2025-05-22 20:20:59 +0800

    memasukkan data 5 buah nama ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main(){
     char nama[5][20];
+    int i;
+
+    for(i=0;i<5;i++){
+        cout<<"Masukkan nama"<<i+1<<" =";
+        cin>>nama[i];
+    }
+
+    for(i=0;i<5;i++){
+        cout<<endl<<"Nama = "<<nama[i];
+    }
     return 0;
 }
