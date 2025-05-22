2ee4635e661cc83395c6ca08ef52565b8c88d17d
Satya Permana Alan <spermana.alan@gmail.com>
2025-05-23 05:51:01 +0800

    ulang proses

--- hallo.cpp
+++ hallo.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 
 int main(){
-    char nama[5][20];
-    int i, banyakNama;
-    cout<<"Berapa nama yang ingin diinput = ";
-    cin>>banyakNama;
-
-    for(i=0;i<banyakNama;i++){
-        cout<<"Masukkan nama"<<i+1<<" =";
-        cin>>nama[i];
-    }
-
-    for(i=0;i<banyakNama;i++){
-        cout<<endl<<"Nama"<< i+1<< "="<<nama[i];
-    }
+    
     return 0;
 }
