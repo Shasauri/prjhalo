d55ec1c179380c714b6f67023b887703c8578dab
Satya Permana Alan <spermana.alan@gmail.com>
2025-05-22 19:58:44 +0800

    inisialisasi array string untuk menampung 5 nama

--- hallo.cpp
+++ hallo.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 
 int main(){
-    cout<<"Saya Satya Permana Alan\n";
-    cout<<"Saya Berasal dari Banjarmasin\n";
-    cout<<"Saya Lahir Pada Tanggal 21 Juli 2007";
-    cout<<"Hanya tes";
-    cout<<"Indonesia Hebat";
+    char nama[5][20];
     return 0;
 }
